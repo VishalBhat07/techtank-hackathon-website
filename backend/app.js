864a68ca3de2832a930104589a913943ca745d1f
VishalBhat07 <vishalbhat21092005@gmail.com>
2025-03-26 19:44:12 +0530

    reverted

--- backend/app.js
+++ backend/app.js
@@ -7,25 +7,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-const allowedOrigins = [
-  "https://www.techtankrvce.com",
-  "https://localhost:5173",
-];
-
 const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"],
+  origin: "*", // Allows all origins (Not recommended for production)
+  methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
+  allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
 };
-
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
+app.options("*", cors());
 app.use(express.json());
 
 // MongoDB Connection with better error handling and options
